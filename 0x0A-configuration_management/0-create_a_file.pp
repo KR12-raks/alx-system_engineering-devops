b20f16e09785b21dd016fe0877d48d45ea3fde53
KR12-raks <koketsorakomane12@gmail.com>
2024-06-28 19:13:51 -0700

    Check_0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,3 +1,9 @@
 #creating a file using Puppet
 
-
+file {'create_sxhool':
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
